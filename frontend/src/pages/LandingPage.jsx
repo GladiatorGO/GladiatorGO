b118b72e0c85d1001a897a7af5124ea44904be98
Christina Peng <christina_peng@brown.edu>
2025-02-01 15:35:01 -0500

    bubble text title

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styles from "./bubble.module.css";
 
 function LandingPage() {
   return (
     <div>
-      <h1>Gladiator Go</h1>
+      <TitleText />
     </div>
   )
 }
+
+const TitleText = () => {
+  return (
+    <h1 className="text-center text-9xl font-thin text-indigo-300">
+      {"Gladiator Go".split("").map((child, idx) => (
+        <span className={styles.hoverText} key={idx}>
+          {child}
+        </span>
+      ))}
+    </h1>
+  );
+};
 
 export default LandingPage
