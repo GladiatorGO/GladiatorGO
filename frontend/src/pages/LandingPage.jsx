dc6cddca69550c84b93bbb92d1bc251b2a712166
Christina Peng <christina_peng@brown.edu>
2025-02-01 15:44:17 -0500

    add signup buttons

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import styles from "./bubble.module.css";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
+  const handlePlayClick = () => {
+    navigate("/main");
+  };
+
   return (
     <div>
       <TitleText />
+      <button onClick={handlePlayClick}>Log In</button>
+      <button onClick={handlePlayClick}>Sign Up</button>
     </div>
-  )
+  );
 }
 
 const TitleText = () => {
   return (
-    <h1 className="text-center text-9xl font-thin text-indigo-300">
-      {"Gladiator Go".split("").map((child, idx) => (
-        <span className={styles.hoverText} key={idx}>
-          {child}
-        </span>
-      ))}
-    </h1>
+    <>
+      <h1 className="text-center text-9xl font-thin text-indigo-300">
+        {"Gladiator Go".split("").map((child, idx) => (
+          <span className={styles.hoverText} key={idx}>
+            {child}
+          </span>
+        ))}
+      </h1>
+    </>
   );
 };
 
